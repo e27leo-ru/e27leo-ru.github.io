ba22014aea9ac84646f421f8fc8022a8819d7ecb
e27leo-ru <e27leo@rambler.ru>
2020-10-11 05:48:39 +0300

    day four

--- delivery-food/js/script.js
+++ delivery-food/js/script.js
@@ -1,1 +1,11 @@
 /* JavaScript - Main Script*/
+const cartButton = document.querySelector('#cart-button');
+const modal = document.querySelector(".modal");
+const close = document.querySelector(".close");
+
+function toggleModal() {
+  modal.classList.toggle("is-open");
+}
+
+cartButton.addEventListener('click', toggleModal);
+close.addEventListener('click', toggleModal);
